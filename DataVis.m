cfe41e3fcb2b6f730c5f9b743ba73ab53e0e5984
J. Caleb Macdonald <72512262+jcmacdonald-codesData@users.noreply.github.com>
2021-05-31 09:35:21 -0500

    Add files via upload

--- DataVis.m
+++ DataVis.m
@@ -7,19 +7,21 @@ StatePops = [4903185,731545,7278717,3017804, 39512223, 5758736, 3565287, 973764,
     3080156, 1359711, 8882190, 2096829, 19453561, 10488084, 762062,56882, 11689100, 3956971,...
     4217737,12801989,3193694,1059361, 5148714, 884659, 6829174, 28995881, 3205958, 623989,106977,...
     8535519, 7614893,1792147,5822434, 578759]';
-
-load('Data3.mat')
-load('USparams.mat')
-load('Dfits.mat')
-Deaths = Dfits(end,:)';
-Data = Data3;
+%load('meds.mat')
+%load('Data3.mat')
+%load('USparams.mat')
+%load('Dfits.mat')
+load('rr.mat')
+%Deaths = Dfits(end,:)';
+Data = rr;
+%Data(37,:) = [];
 R0s = Data(:,1);
 psis = Data(:,2);
 alphas = Data(:,3);
 captures = Data(:,5);
 infecteds = Data(:,7);
-F0BS = Data(:,11);
-Mortality = Data(:,12);
+F0BS = Data(:,9);
+Mortality = Data(:,15);
 %Sizes = Data(:,13);
 
 scatter(R0s,psis,30,Mortality,'filled') 
@@ -29,7 +31,7 @@ set(gca,'FontSize',16)
 ax = gca;
 %ax.XDir = 'reverse';
 %view(-31,14)
-ylabel('\psi')
+ylabel('lockdown factor, \psi')
 xlabel('R_0')
 xlim([min(R0s),max(R0s)])
 %colormap(flipud(stoplight(64)))
@@ -39,16 +41,16 @@ cb.Label.String = 'Mortality (% population)';
 
 
 figure
-scatter(alphas,psis,30,Mortality,'filled') 
+scatter(log(2)./alphas,psis,30,Mortality,'filled') 
 
 
 % draw the scatter plot
 ax = gca;
 %ax.XDir = 'reverse';
 %view(-31,14)
-ylabel('\psi')
-xlabel('\alpha')
-xlim([min(alphas),max(alphas)])
+ylabel('lockdown factor')
+xlabel('fatigue half-life, log(2)/\alpha (days)')
+xlim([min(log(2)./alphas),max(log(2)./alphas)])
 colormap(jet(16))
 cb = colorbar;                                     % create and label the colorbar
 cb.Label.String = 'Mortality (% population)';
@@ -88,18 +90,18 @@ set(gca,'FontSize',16)
 
 figure
 hold on
-scatter(R0s,psis,30,alphas,'filled')    % draw the scatter plot
+scatter(R0s,psis,30,log(2)./alphas,'filled')    % draw the scatter plot
 hold off
 
 ax = gca;
 %ax.XDir = 'reverse';
 %view(-31,14)
 xlabel('R_0')
-ylabel('\psi')
-colormap(jet(16))
+ylabel('lockdown factor')
+colormap(fliplr(jet(16)))
 xlim([min(R0s),max(R0s)])
 cb = colorbar;                                     % create and label the colorbar
-cb.Label.String = 'Lockdown Fatigue Measure';
+cb.Label.String = 'fatige half-life (days)';
 set(gca,'FontSize',16)
 figure
 hold on
@@ -116,43 +118,43 @@ ylabel('\psi')
 colormap(jet(16))
 xlim([min(R0s),max(R0s)])
 cb = colorbar;                                     % create and label the colorbar
-cb.Label.String = 'True Cum. Case Estimate (% pop.)';
+cb.Label.String = 'Cum. Case Estimate (% pop.)';
 set(gca,'FontSize',16)
 figure
 hold on
 
-scatter(alphas,psis,30,F0BS,'filled')
+scatter(log(2)./alphas,psis,30,F0BS,'filled')
 hold off
 
 % draw the scatter plot
 ax = gca;
 %ax.XDir = 'reverse';
 %view(-31,14)
-xlabel('\alpha')
-ylabel('\psi')
+xlabel('fatigue half-life (days)')
+ylabel('lockdown factor')
 colormap(jet(16))
-xlim([min(alphas),max(alphas)])
+xlim([min(log(2)./alphas),max(log(2)./alphas)])
 cb = colorbar;                                     % create and label the colorbar
-cb.Label.String = 'True Cum. Case Estimate (% pop.)';
+cb.Label.String = 'Cum. Case Estimate (% pop.)';
 set(gca,'FontSize',16)
 figure
 hold on
 
-scatter(alphas,psis,30,R0s,'filled')
+scatter(log(2)./alphas,psis,30,R0s,'filled')
 hold off
 
 % draw the scatter plot
 ax = gca;
 %ax.XDir = 'reverse';
 %view(-31,14)
-xlabel('\alpha')
-ylabel('\psi')
+xlabel('fatigue half-life (days)')
+ylabel('lockdown factor')
 colormap(jet(16))
-xlim([min(alphas),max(alphas)])
+xlim([min(log(2)./alphas),max(log(2)./alphas)])
 cb = colorbar;                                     % create and label the colorbar
 cb.Label.String = 'Reproduction Number';
 set(gca,'FontSize',16)
-load('R0s.mat')
+load('R0sRev.mat')
 VarRepos = R0s;
 for j = 1:length(VarRepos(:,1))
     for k = 1:55
@@ -164,23 +166,23 @@ end
     
 figure
 hold on
-h=fill([77,130,130,77],[0,0,6,6],'k','LineStyle','none');
-    h.FaceAlpha=0.1;
-plot(0:1:130,ones(1,131),'k-.','linewidth',1.25)
+%h=fill([77,130,130,77],[0,0,6,6],'k','LineStyle','none');
+%    h.FaceAlpha=0.1
 for j = 1:55
     plot(0:1:length(VarRepos(:,1))-1,VarRepos(:,j))
 end
+plot(0:1:130,ones(1,131),'k-.','linewidth',1.25)
 hold off
 xlim([0 130])
 xticks(round(linspace(0,length(VarRepos(:,1))-1,10)))
-xlabel('Days Since First Reported Case up to May 31')
-ylabel('Time Variable Reproduction Number')
-legend({'Stay at Home Order(s) in Effect'},'FontSize',10)
+xlabel('Days Since First Rep. Case up to May 31')
+ylabel('Effective Repro. Number, R_e')
+%legend({'Stay at Home Order(s) in Effect'},'FontSize',10)
 set(gca,'FontSize',16)
 load('Cfits.mat')
 load('TCTfits.mat')
-load('rhos.mat')
-load('Tests.mat')
+load('rhosRev.mat')
+load('TetsRevision.mat')
 for j = 1:55
     for k = 1:length(rhos(:,1))
     Temp(k,j) = rhos(k,j);
@@ -207,8 +209,8 @@ end
 
 figure
 hold on
-h=fill([77,130,130,77],[0,0,max(max(rhos))*1.05,max(max(rhos))*1.05],'k','LineStyle','none');
-    h.FaceAlpha=0.1;
+%h=fill([77,130,130,77],[0,0,max(max(rhos))*1.05,max(max(rhos))*1.05],'k','LineStyle','none');
+%    h.FaceAlpha=0.1;
 for j = 1:55
     plot(0:1:length(VarRepos(:,1))-1,Temp(:,j))
 end
@@ -217,9 +219,9 @@ xlim([0 130])
 ylim([.95*.03,max(max(rhos))*1.05])
 yticks(round(linspace(.05,max(max(rhos)),11),2))
 xticks(round(linspace(0,length(VarRepos(:,1))-1,10)))
-xlabel('Days Since First Reported Case up to May 31')
+xlabel('Days Since First Rep. Case up to May 31')
 ylabel('Ascertainment Rate')
-legend({'Stay at Home Order(s) in Effect'},'FontSize',10)
+%legend({'Stay at Home Order(s) in Effect'},'FontSize',10)
 set(gca,'FontSize',16)
 
 
@@ -228,16 +230,16 @@ format long
 
 figure
 hold on
-h=fill([77,130,130,77],[0,0,.5,.5],'k','LineStyle','none');
-    h.FaceAlpha=0.1;
+%h=fill([77,130,130,77],[0,0,.5,.5],'k','LineStyle','none');
+ %   h.FaceAlpha=0.1;
 for j = 1:55
     plot(0:1:length(Tests(:,1))-1,Temp2(:,j))
 end
 hold off
 set(gca,'FontSize',16)
-legend({'Stay at Home Order(s) in Effect'},'FontSize',10)
+%legend({'Stay at Home Order(s) in Effect'},'FontSize',10)
 xticks(round(linspace(0,length(VarRepos(:,1))-1,10)))
-ylabel('Daily Tests (% State Population)')
+ylabel('Daily Tests (% State Pop.)')
 xlabel('Days Since First Reproted Case up to May 31')
 xlim([0 130])
 
